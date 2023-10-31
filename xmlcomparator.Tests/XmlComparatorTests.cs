df49c8d2d4e9efd2bc50d0b9a2275b5de967dba5
Arturro <arturro_1996@o2.pl>
2023-10-31 14:12:26 +0100

    Creating Tests Examples

---
+++ xmlcomparator.Tests/XmlComparatorTests.cs
@@ -0,0 +1,34 @@
+namespace xmlcomparator.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CompareLineByLine_ShouldDrawCorrectReport_WhenFileAreTheSame()
+    {
+
+    }
+
+    [Fact]
+    public void CompareLineByLine_ShouldDrawCorrectReport_WhenFileAreTheSame_UsedWithOnlyLinesOptions()
+    {
+
+    }
+
+    [Fact]
+    public void CompareLineByLine_ShouldDrawCorrectReport_WhenFileAreDiffrent()
+    {
+
+    }
+
+    [Fact]
+    public void CompareLineByLine_ShouldDrawCorrectReport_WhenFileAreDiffrent_UsedWithOnlyLinesOptions()
+    {
+
+    }
+
+    [Fact]
+    public void CompareLineByLine_ShouldDrawFileReport_WhenFileAreNotCorrect()
+    {
+
+    }
+}
