51c9b9eb4b396928898dd1c0c00634bd7fd7a4d1
Arturro <arturro_1996@o2.pl>
2023-11-03 23:24:30 +0100

    XmlController first commit

--- xmlcomparator/XmlComparatorController.cs
+++ xmlcomparator/XmlComparatorController.cs
@@ -4,6 +4,28 @@ public class XmlComparatorController
 {
     public XmlComparatorController(string[] args)
     {
-        new XmlComparator("FileToTest3.xml","FileToTest2.xml").CompareByElement(false);
+       if (args.Length == 0)
+       {
+            new XmlComparator("FileToTest3.xml","FileToTest2.xml").CompareByElement(false);
+            
+       }else {
+            try{
+                if(args[0] == "-t" || args[0] == "--type")
+                {
+                    if(args[1] == "line")
+                    {
+                        new XmlComparator().CompareLineByLine(false);
+                    }
+                    if(args[1] == "elements")
+                    {
+                        new XmlComparator().CompareByElement(false);
+                    }
+                }
+            }catch(IndexOutOfRangeException e)
+            {
+                Console.Write("Wrongs comends, use -h or --help to see correct format");
+            }
+       }
+
     }
 }
