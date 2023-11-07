2e7b2476d3c95bd7997735f52f800cdec446ef8a
Arturro <arturro_1996@o2.pl>
2023-11-07 18:02:08 +0100

    Write tests case scenario for XmlComparatorController

---
+++ xmlcomparator.Tests/XmlComparatorControllerTests.cs
@@ -0,0 +1,32 @@
+﻿namespace xmlcomparator.Tests;
+
+public class XmlComparatorControllerTests
+{
+    [Fact]
+    public void Constructor_WhenGivenPathArgumentIsNotValid_ShouldUseAddErrorFunction()
+    {
+
+    }
+
+    [Fact]
+    public void Constructor_WhenGivenPathArgumentIsValid_ShouldUseSetFilesPathFunction()
+    {
+
+    }
+
+    [Fact]
+    public void Constructor_WhenGivenReportArgument_ShouldUseSetReportPathFunction()
+    {
+
+    }
+    [Fact]
+    public void Constructor_WhenGivenValidTypeArgument_ShouldUseChangeTypeFunction()
+    {
+
+    }
+    [Fact]
+    public void Constructor_WhenGivenNotValidTypeArgument_ShouldUseAddError()
+    {
+
+    }
+}
